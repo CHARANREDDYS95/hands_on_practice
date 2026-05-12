1cb912e13a1c3e72f35c9f32f6e8692dabe8e1a5
CHARANREDDYS95 <charanreddymittapalli@gmail.com>
2026-05-12 12:23:06 +0530

    file 1

---
+++ DATA1.sql
@@ -0,0 +1,9 @@
+--------------------------------------------------------
+--  File created - Thursday-May-07-2026   
+--------------------------------------------------------
+REM INSERTING into TEAM40_SHEKAR.PRODUCT
+SET DEFINE OFF;
+Insert into TEAM40_SHEKAR.PRODUCT (P_ID,P_NAME,P_FAMILY,PRICE,COST,LAUNCH_DATE) values (100,'Marker','Stationary',25,22,to_date('15-01-08','DD-MM-RR'));
+Insert into TEAM40_SHEKAR.PRODUCT (P_ID,P_NAME,P_FAMILY,PRICE,COST,LAUNCH_DATE) values (101,'Mouse','Computer',450,350,to_date('16-04-09','DD-MM-RR'));
+Insert into TEAM40_SHEKAR.PRODUCT (P_ID,P_NAME,P_FAMILY,PRICE,COST,LAUNCH_DATE) values (102,'White Board','Stationary',450,375,to_date('20-08-10','DD-MM-RR'));
+Insert into TEAM40_SHEKAR.PRODUCT (P_ID,P_NAME,P_FAMILY,PRICE,COST,LAUNCH_DATE) values (103,'SONY Vaio','Computer',35000,42000,to_date('10-09-10','DD-MM-RR'));
